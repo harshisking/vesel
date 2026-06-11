570b703b7e01f5a05b03e9945b6f9c53da16e37a
harshisking <barotharsh2852009@gmail.com>
2026-06-11 18:46:18 +0530

    Fix payload deserialization

--- src/vesel/io.py
+++ src/vesel/io.py
@@ -70,7 +70,7 @@ class VeselIO:
         payload = data[12:(12+length)]
 
         return VeselFile(
-            Header(version),
+            Header(version=version, payload_length=length),
             payload
         )
         
